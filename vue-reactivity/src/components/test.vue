8da02b3f16ad8c869e4977176286ce68c71a574b
pengshi2 <boogiesnoogawoot@gmail.com>
2023-03-08 10:14:12 -0500

    a

---
+++ vue-reactivity/src/components/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">BANSAJADASSDASD</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "beans",
+    };
+  },
+};
+</script>
+
+<style></style>
--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
+import test from "../components/test.vue";
 </script>
 
 <template>
+  <h2>{{ message }}</h2>
   <main>
     <TheWelcome />
+    <test />
   </main>
 </template>
