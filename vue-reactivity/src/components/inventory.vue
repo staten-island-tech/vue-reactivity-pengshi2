6ba2bfa446d08f5e19393064447ffe72a06e190a
pengshi2 <boogiesnoogawoot@gmail.com>
2023-03-14 10:20:39 -0400

    a

--- vue-reactivity/src/components/inventory.vue
+++ vue-reactivity/src/components/inventory.vue
@@ -1,19 +1,19 @@
 <script>
 export default {
   data() {
-  return {
-    list:[
-      {id: 1, name: 'Apple'},
-      {id: 2, name: 'Pear'},
-      {id: 3, name: 'Cherry'},
-    ]
+    return {
+      list: [
+        { id: 1, name: "Cherry MX Red", price: 0.5 },
+        { id: 2, name: "Cherry MX Brown", price: 0.5 },
+        { id: 3, name: "Cherry MX Blue", price: 0.5 },
+      ],
+    };
   },
-}
 };
 </script>
 
 <template>
-  <div></div>
+  <div>HI IM A DIV</div>
 </template>
 
 <style></style>
