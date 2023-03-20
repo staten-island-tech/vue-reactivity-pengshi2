f8567fe43f82ba759c54e284aff0c74ee06b694f
pengshi2 <boogiesnoogawoot@gmail.com>
2023-03-20 10:14:09 -0400

    v

---
+++ vue-reactivity/src/components/items.js
@@ -0,0 +1,70 @@
+const items = [
+  {
+    id: 1,
+    name: "Gateron Red",
+    price: 0.35,
+    type: "Linear",
+    Description:
+      "Perfect entry level linear switches for people just getting into mechanichal keyboards. They are cheap and smooth.",
+  },
+  {
+    id: 2,
+    name: "Kaihl Box Creams",
+    price: 1.5,
+    Type: "Linear",
+    Description: "",
+  },
+  {
+    id: 3,
+    name: "Kaihl Box Jades",
+    price: 0.4,
+    Type: "Clicky",
+    Description: "",
+  },
+  { id: 4, name: "Zealios V2", price: 1.1, Type: "Tactile", Description: "" },
+  {
+    id: 5,
+    name: "Gateron Yellows",
+    price: 0.2,
+    Type: "Linear",
+    Description:
+      "Linear switches for people looking for quality switches while on a tight budget. Well renowed for their exceptional performance for price.",
+  },
+  {
+    id: 6,
+    name: "Gateron Ink Black V2",
+    price: 0.85,
+    Type: "Linear",
+    Description: "",
+  },
+  {
+    id: 7,
+    name: "Drop Holy Panda X",
+    price: 1,
+    Type: "Tactile",
+    Description: "",
+  },
+  {
+    id: 8,
+    name: "Kaihl Box Royal",
+    price: 0.2,
+    Type: "Tactile",
+    Description: "",
+  },
+  {
+    id: 9,
+    name: "Novelkeys X Kaihl Sherbert",
+    price: 0.45,
+    Type: "Clicky",
+    Description: "",
+  },
+  {
+    id: 10,
+    name: "Roselios Silent",
+    price: 1.3,
+    Type: "Linear",
+    Description: "",
+  },
+];
+
+export default items;
--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script>
 export default {
+  name: "HomeView",
   components: {},
+  data() {
+    return {
+      items: items,
+    };
+  },
 };
 </script>
 
 <template>
-  <li v-for="item in items">
+  <li v-for="item in list">
     {{ item.name }}
   </li>
 </template>
